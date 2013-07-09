6b2210c65739e0637b569e0e443dba96732edd0a
U-VehpuS-PC\VehpuS <VehpuS@VehpuS-PC.(none)>
2013-07-09 04:56:59 +0300

    redirected web.js to index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+		var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	answer = fs.readFileSync(index.html).toString()
+  	response.send(answer);
 });
 
 var port = process.env.PORT || 5000;
