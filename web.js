0a43a130cb23602b467fd2c1fde6525917419cec
U-VehpuS-PC\VehpuS <VehpuS@VehpuS-PC.(none)>
2013-07-19 07:44:09 +0300

    Started adding styles to the site. In the middle of implementing the top navbar

--- web.js
+++ web.js
@@ -2,12 +2,19 @@ var fs = require('fs');
 
 var express = require('express');
 
-		var app = express.createServer(express.logger());
+
+
+var app = express.createServer(express.logger());
+
+app.use("/assets", express.static(__dirname + '/assets'));
+//app.use("/styles", express.static(__dirname + '/styles'));
 
 app.get('/', function(request, response) {
 	answer = fs.readFileSync('index.html').toString();
+	//answer = fs.readFileSync(request).toString();
 	response.send(answer);
 });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
