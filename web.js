c4f0de517ac0f7d87102f0bde15b25bd71ad92fe
VehpuS <vehpus@gmail.com>
2013-07-23 05:43:26 +0300

    Added content to the page - a carousel of pictures, text, disabled irrelevant links and fixed up the info

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/assets/img", express.static(__dirname + '/assets/img'));
 //app.use("/styles", express.static(__dirname + '/styles'));
 
 app.get('/', function(request, response) {
